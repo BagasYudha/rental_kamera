4e53332439ad04f44261e1211235f43f5f25d5fa
Bagas Yudha <bayud.kematian@gmail.com>
2024-03-29 00:05:11 +0700

    Membuat navbar

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="w-full bg-gray-600">
+        <nav>
+            <div class="bg-gray-700">
+                <img src="Media/Logo-hitam.png" alt="Logo Kary" class="">
+            </div>
+            <div class="flex">
+                <ul>
+                    <li><a href="#">Beranda</a></li>
+                    <li><a href="#">Beranda</a></li>
+                    <li><a href="#">Beranda</a></li>
+                    <li><a href="#">Beranda</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+</body>
+
+</html>
