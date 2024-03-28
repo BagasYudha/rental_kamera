e5f7684e307d9ed4fa5e35103eb4cabcd598132b
Bagas Yudha <bayud.kematian@gmail.com>
2024-03-28 17:19:58 +0700

    Konfigurasi route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/adminlog', function () {
+    return view('AdminLogin');
+});
+
+Route::get('/admin', function () {
+    return view('Admin');
+});
