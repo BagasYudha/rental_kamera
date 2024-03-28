18a6aa93daaa16c67a7729d82ff6c27d7c7e51dc
kidiepie <156444129+kiddiepiee@users.noreply.github.com>
2024-03-28 15:13:27 +0700

    Create login.php

---
+++ config/login.php
@@ -0,0 +1,10 @@
+<?php
+
+// Simpan ID Admin dan password dalam file ini
+$idAdmin = "admin";
+$password = "admin";
+
+// Outputkan ID Admin dan password dalam format yang bisa dibaca oleh JavaScript
+echo $idAdmin . "\n" . $password;
+
+?>
