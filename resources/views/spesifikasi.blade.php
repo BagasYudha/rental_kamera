c6682a2f0a2e3cfa81903717c7110de70c2ffdda
rkaaaaaa <noobteam096@gmail.com>
2024-03-28 18:11:42 +0700

    menambahkan file spesifikasi.blade.php

---
+++ resources/views/spesifikasi.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KaryCam - Canon EOS250D</title>
+    <link rel="stylesheet" href="spesifikasi.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="./mboh/KaryCam.png" alt="CaryCam" width="100" height="75"> 
+        </div>
+    </header>
+    <main>
+        <div class="NamCam"></div>
+            <h1>CannonEOS250D</h1>
+        <div class="container">
+            <div class="camera">
+            <img src="./mboh/EOS250.png" alt="Canon" width="250" height="225">
+            </div>
+            <div class="Harga">
+            <h3>Rp. 50.000/Hari</h3>
+            <a class="whatsapp-link" href="https://api.whatsapp.com/send?phone=628123456789&text=Halo,%20saya%20tertarik%20dengan%20sewa%20Canon%20EOS%20250D%20RP.%2050.000%20per%20hari.">
+                <img src="./mboh/IconWA.png" alt="WhatsApp icon">
+            </a>
+            </div>
+            <div class="spesifikasi">
+            <h1>Spesifikasi</h1>
+            <ul>
+                <li>Image Sensor
+                    - Type: 22.3 mm x 14.9 mm CMOS
+                    - Effective Pixels: Approx. 24.10 megapixels
+                    - Total Pixels: Approx. 25.80 megapixels
+                    - Aspect Ratio: 3:2
+                    - Low-Pass Filter: Built-in/Fixed
+                    - Sensor Cleaning: EOS integrated cleaning system
+                    - Colour Filter Type: Primary Colour
+                    
+                    Image Processor
+                    - Type: DIGIC 8
+                    
+                    Lens
+                    - Lens Mount: EF and EF-S mount
+                    - Focal Length: Approx. 1.6x the focal length of the lens
+                    - Image Stabilisation: Optical Image Stabilizer on compatible lenses
+                    
+                    Focusing
+                    - Type:
+                    - Via optical viewfinder: TTL secondary image-forming phase-difference detection system with AF-dedicated sensor.
+                    - Via LiveView on LCD screen: Dual Pixel CMOS AF System. Phase detection pixels built onto imaging sensor
+                    - AF System / Points:
+                    - Via optical viewfinder: 9-point Center AF point is AF cross-type supporting f/5.6. Center AF point is vertical line-sensitive supporting f/2.8. Other AF points are vertical line-sensitive or horizontal line-sensitive AF supporting f/5.6.
+                    - Via LiveView on LCD screen: Maximum 143 AF points via camera automatic selection. Freely position 1 AF point/1 AF Zone max 3,975 points via manual selection
+                    - AF working range:
+                    - Via optical viewfinder: EV -0.5 – 18 (center AF point) EV 0.5 – 18 (other AF points)
+                    - Via LiveView on LCD screen: EV -4 – 18 (f/1.2 at 23 °C & ISO 100) with One-Shot AF
+                    - AF Modes: AI Focus, One Shot, AI Servo (AI Servo II algorithm)
+                    - AF Point Selection:
+                    - Via optical viewfinder:
+                        - Automatic selection: 9 point AF
+                        - Manual selection: Single point AF
+                    - Selected AF point display: Indicated by a transmissive LCD in viewfinder and Quick Control screen
+                    - AF Lock: Locked when shutter button is pressed halfway in One-Shot AF mode or AF-ON Button is pressed.
+                    - AF Assist Beam: Emitted by optional dedicated Speedlite
+                    - Manual Focus: Selected on lens
+                    
+                    Exposure Control
+                    - Metering modes:
+                    - Via optical viewfinder: 63-zone dual-layer metering sensor with Evaluative, partial, spot (centre only), and centre-weighted average available
+                    - Via LiveView on LCD screen:
+                        - Evaluative metering (384 zones)
+                        - Partial metering (approx. 5.5 % of LCD screen)
+                        - Spot metering (approx. 3.7 % of LCD screen)
+                        - Center-weighted average metering
+                    - Metering Range:
+                    - Via optical viewfinder: EV 1 – 20 (at 23 °C with 50 mm f/1.4 lens ISO 100)
+                    - Via LiveView on LCD screen: EV -2 – 20 (at 23 °C, ISO 100, evaluative metering)
+                    - AE Lock:
+                    - Auto: In One-shot AF mode with evaluative metering exposure is locked when focus is achieved.
+                    - Manual: By AE Lock Button in creative zone modes.
+                    - Exposure Compensation: +/-5 EV in 1/3 or 1/2 stop increments (can be combined with AEB).
+                    - AEB: 3 shots +/-2 EV 1/3 or 1/2 stop increments
+                    - ISO Sensitivity:
+                    - Auto (100-25600), 100-25600 (in whole stop increments)
+                    - ISO can be expanded to H: 51200
+                    - During Movie shooting: Auto (100-12800), 100-12800 (in 1/3-stop or whole stop increments) ISO can be expanded to H: 25600. 4K Movie Max. 6400
+                    
+                    Shutter
+                    - Type: Electronically-controlled focal-plane shutter
+                    - Speed: 30-1/4000 sec (1/2 or 1/3 stop increments), Bulb (Total shutter speed range. Available range varies by shooting mode)</li>
+            </ul>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
