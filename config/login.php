5905b7404a043db252fff49023cc5c75ba23e43e
Bagas Yudha <bayud.kematian@gmail.com>
2024-03-28 23:15:48 +0700

    Memperbaiki error login.php

--- config/login.php
+++ config/login.php
@@ -5,6 +5,6 @@ $idAdmin = "admin";
 $password = "admin";
 
 // Outputkan ID Admin dan password dalam format yang bisa dibaca oleh JavaScript
-echo $idAdmin . "\n" . $password;
 
-?>
+//ini errornya tadi
+//echo $idAdmin . "\n" . $password;
