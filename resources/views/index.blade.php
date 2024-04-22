05abaaa5908a1d0cfd11ba534ad04d3503dd31b6
Bagas Yudha <bayud.kematian@gmail.com>
2024-04-22 10:13:37 +0700

    Progress 21/04 menambahkan feather icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
+    <script src="https://unpkg.com/feather-icons"></script>
+
     @vite('resources/css/app.css')
 
 </head>
@@ -29,8 +31,8 @@
             <!-- div teks -->
             <div class="md:mt-36">
                 <h3 class="text-2xl">Selamat datang di</h3>
-                <h1 class="font-semibold text-7xl" style="background: linear-gradient(to right, #4F46E5, #02CCFE); -webkit-background-clip: text; -webkit-text-fill-color: transparent; background-clip: text; color: transparent; ">KaryCam!</h1>
-                <p class="mt-3 text-lg">
+                <h1 class="font-semibold text-8xl" style="background: linear-gradient(to right, #4F46E5, #02CCFE); -webkit-background-clip: text; -webkit-text-fill-color: transparent; background-clip: text; color: transparent; ">KaryCam!</h1>
+                <p class="mt-3 text-xl">
                     Kami merupakan perusahaan jasa rental kamera <br>
                     yang didirikan oleh Adi Alam, Bagas Yudha, Bintang Raka, <br>
                     dan Muhammand Miftahul Karim.
@@ -54,14 +56,46 @@
 
     <!-- CONTAIN 2 -->
     <section id="Contact" class="px-[50px] mt-[600px]">
-        <div class="font-bold text-5xl text-black">
+        <div class="font-semibold text-6xl text-black">
             <h1 class="">Ada Project ?</h1>
             <h1 class="mt-3">Konsultasi peralatan ?</h1>
             <h1 class="mt-3">Mau sewa gear produksi ?</h1>
             <h1 class="mt-3">Atau ngajak mimin ngopi ?</h1>
         </div>
+        <!-- Footer Start -->
+        <footer class="my-24">
+            <div class="flex">
+                <!-- Contact -->
+                <div class="flex border-r-[5px] border-r-[#8152FB] w-60">
+                    <div class="flex items-center ">
+                        <i class="bordertext-black w-16 h-16" data-feather="message-circle"></i>
+                        <div class="ml-3 w-32 items-center">
+                            <h3>Kalo mau chat klik disini!</h3>
+                        </div>
+                    </div>
+                </div>
+                <!-- Location -->
+                <!-- Contact -->
+                <div class="flex border-r-[5px] border-r-[#8152FB] w-60">
+                    <div class="flex items-center ">
+                        <i class="bordertext-black w-16 h-16" data-feather="message-circle"></i>
+                        <div class="ml-3 w-32 items-center">
+                            <h3>Kalo mau chat klik disini!</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Contact End -->
+            <div></div>
+        </footer>
+        <!-- Footer End -->
     </section>
+    <!-- CONTAIN 2 END -->
+    <script>
+        feather.replace();
+    </script>
 </body>
-<!-- CONTAIN 2 END -->
+
 
 </html>
