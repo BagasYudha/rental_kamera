6d84990589f6a686126b3db6a18f74fcca09f926
Bagas Yudha <bayud.kematian@gmail.com>
2024-04-23 12:09:13 +0700

    Progress 22/04 animasi navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -14,15 +14,17 @@
 </head>
 
 <body>
-    <nav class="px-[20px] font-['Poppins'] justify-between items-center flex w-full h-20 bg-[#E8FFFB]">
+    <nav class="px-[20px] font-['Poppins'] justify-between items-center flex w-full h-20 bg-[#e8fffb67] backdrop-blur-sm border-b-[1px] border-white">
         <div class="justify-start w-56 h-full bg-cover bg-center" style="background-image: url('Media/Logo-hitam.png')">
         </div>
         <div class="flex justify-end w-full h-full items-center font-sans">
-            <a href="#Home" class="font-bold text-black hover:text-blue-900 px-3 py-2 mx-3">Beranda</a>
-            <a href="#" class="font-bold text-black hover:text-blue-900 px-3 py-2 mx-3">Rental</a>
-            <a href="/spesifikasi" class="font-bold text-black hover:text-blue-900 px-3 py-2 mx-3">Spesifikasi</a>
-            <a href="#Contact" class="font-bold text-black hover:text-blue-900 px-3 py-2 mx-3">Hubungi Kami</a>
+            <a href="/" class="link font-bold px-3 py-2 mx-3">Beranda</a>
+            <a href="#" class="link font-bold px-3 py-2 mx-3">Rental</a>
+            <a href="/spesifikasi" class="link font-bold px-3 py-2 mx-3">Spesifikasi</a>
+            <a href="#Contact" class="link font-bold px-3 py-2 mx-3">Hubungi Kami</a>
         </div>
+
+
     </nav>
 
 
