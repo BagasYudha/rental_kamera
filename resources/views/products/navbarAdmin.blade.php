fb46c7f4848e292e5efb97ee2caab94e759f3922
Bagas Yudha <bayud.kematian@gmail.com>
2024-05-08 11:06:16 +0700

    Konfigurasi dengan database ke hosting

---
+++ resources/views/products/navbarAdmin.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="px-[20px] font-['Poppins'] justify-between items-center flex w-full h-20 bg-[#e8fffb67] backdrop-blur-sm border-b-[1px] border-white">
+        <div class="justify-start w-56 h-full bg-cover bg-center" style="background-image: url('Media/Logo-hitam.png')">
+        </div>
+        <div class="flex justify-end w-full h-full items-center font-sans">
+            <a href="/products" class="link font-bold px-3 py-2 mx-3">Dashboard</a>
+            <a href="/Transaksi" class="link font-bold px-3 py-2 mx-3">Transaksi</a>
+        </div>
+
+    </nav>
+
+
+</body>
+
+</html>
