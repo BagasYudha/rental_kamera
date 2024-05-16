18fd8c7a55a9dcdd033fdfc81d698676467fc4d7
rkaaaaaa <noobteam096@gmail.com>
2024-05-16 12:49:20 +0700

    Memperbaiki transaksi.blade.php

--- resources/views/Transaksi.blade.php
+++ resources/views/Transaksi.blade.php
@@ -5,15 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transaksi</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap">
     <link rel="stylesheet" href="Transaksi.css">
 
 </head>
 
 <body>
+    <nav class="px-[20px] font-['Poppins'] justify-between items-center flex w-full h-20 bg-[#e8fffb67] backdrop-blur-sm border-b-[1px] border-white">
+        <div class="justify-start w-56 h-full bg-cover bg-center" style="background-image: url('Media/Logo-hitam.png')">
+        </div>
+        <div class="flex justify-end w-full h-full items-center font-sans">
+            <a href="/products" class="link font-bold px-3 py-2 mx-3">Dashboard</a>
+            <a href="/Transaksi" class="link font-bold px-3 py-2 mx-3">Transaksi</a>
+        </div>
+    </nav>
+
     <div class="container">
         <div class="header-container">
-            <h1>Transaksi</h1>
+            <h1 style="font-size: 2em;">Transaksi</h1>
             <a href="{{ route('transaksi.create') }}" class="TambahTransaksi">Tambah Transaksi</a>
         </div>
         <hr>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ForgotPassword;
 use App\Http\Controllers\TransaksiController;
-use App\Http\Controllers\TransaksiController;
-use App\Http\Controllers\Auth\ForgotPassword;
+use App\Http\Controllers\AdminController;
+
 
 Route::get('/', function () {
     return view('index');
