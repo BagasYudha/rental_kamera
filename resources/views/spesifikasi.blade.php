050b322d3306c250f22b9d6982686fc084dd632f
Bagas Yudha <bayud.kematian@gmail.com>
2024-06-10 04:40:40 +0700

    membuat tampilan spesifikasi dan responsive

--- resources/views/spesifikasi.blade.php
+++ resources/views/spesifikasi.blade.php
@@ -13,41 +13,35 @@
     <script src="https://unpkg.com/feather-icons"></script>
 </head>
 
-<body class="bg-[#E8FFFB] font-['Poppins'] w-full overflow-x-hidden">
-
+<body class="bg-[#E8FFFB] font-['Poppins'] w-full">
     <!-- NAVBAR -->
     <div class="fixed w-full z-20">
-        <nav class="px-[20px] font-['Poppins'] justify-between items-center flex w-full h-20 bg-[#e8fffb67] backdrop-blur-sm border-b-[1px] border-white">
-            <a href="/adminlog" class="justify-start w-56 h-full bg-cover bg-center" style="background-image: url('Media/Logo-hitam.png')">
-            </a>
-            <div class="flex justify-end w-full h-full items-center font-sans">
-                <a href="/" class="link font-bold px-3 py-2 mx-3">Beranda</a>
-                <a href="/rental" class="link font-bold px-3 py-2 mx-3">Rental</a>
-                <a href="/#Contact" class="link font-bold px-3 py-2 mx-3">Hubungi Kami</a>
-            </div>
-        </nav>
+        @include('navbar')
     </div>
     <!-- NAVBAR END -->
 
-    <div class="container flex">
-        <div class="left flex flex-col items-start p-5 fixed top-0 left-0">
-            <h3 class="NamaBarang text-2xl font-semibold absolute top-20 left-24">{{ $product->nama_item }}</h3>
-            <img src="{{ asset('/storage/products/' . $product->image) }}" alt="{{ $product->nama_item }}" class="mt-32 ml-16 border-2 border-purple-600 p-5 rounded-lg max-w-xs">
-            <div class="harga mt-4 ml-16">
-                Tersedia {{ $product->stock }}<br>
-                {{ "Rp " . number_format($product->price,2,',','.') }}
-            </div>
-            <div class="logo-whatsapp flex items-center bg-purple-600 w-52 h-12 ml-14 rounded-full mt-4">
-                <a href="https://wa.me/6281331571129" class="whatsapp-link flex items-center" target="_blank">
-                    <img src="./Media/Icon/Whatsapp.png" alt="WhatsApp" class="w-16 ml-2 mb-32 border-none"><span class="whatsapp-text text-xl ml-1 font-bold text-white">Booking</span>
+    <section class="md:flex w-full justify-between px-8 relative">
+        <div>
+            <div class="w-full md:w-1/3 p-8 md:mt-12 md:fixed ">
+                <h3 class="text-2xl my-3 font-semibold md:text-left text-center">{{ $product->nama_item }}</h3>
+                <div class="border-2 border-purple-600 rounded-lg">
+                    <img src="{{ asset('/storage/products/' . $product->image) }}" alt="{{ $product->nama_item }}">
+                </div>
+                <div class="my-2 md:text-left text-center  md:text-lg text-sm w-full">
+                    <h1>Tersedia {{ $product->stock }}</h1>
+                    <h1>{{ "Rp " . number_format($product->price,2,',','.') }}</h1>
+                </div>
+                <a href="https://wa.me/6281331571129" class="flex items-center justify-center bg-purple-600 rounded-full" target="_blank">
+                    <img src="../Media/Icon/Whatsapp.png" alt="WhatsApp" class="w-8 md:w-12">
+                    <div class=" md:text-lg text-sm font-bold text-white">Booking</div>
                 </a>
             </div>
         </div>
-        <div class="right flex-2 p-12 bg-white overflow-y-auto ml-[600px] mt-24 relative rounded-lg">
+
+        <div class="bg-white overflow-y-auto text-xs md:text-base rounded-lg w-full md:w-2/3 right-0 p-8 md:mt-20">
             <p>{{ $product->description }}</p>
         </div>
-    </div>
-    </main>
+    </section>
 </body>
 
 </html>
