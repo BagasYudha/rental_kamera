b6e0c8685b77940af77ca6580d38e92e375443d0
Karim28com <muhammad281004@gmail.com>
2024-06-09 21:50:37 +0700

    tampilan halaman produk

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
+<body style="background: lightcyan"></body>
 <body>
 
     <div class="container mt-5 mb-5">
