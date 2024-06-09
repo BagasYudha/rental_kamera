ec775a4b213a775ea6ec23e750d72972bbdc0507
Bagas Yudha <bayud.kematian@gmail.com>
2024-06-09 20:28:01 +0700

    menyelesaikan rental dan set item klik spesifikasi

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -10,7 +10,11 @@
 </head>
 
 <body>
-
+    <!-- NAVBAR -->
+    <div class="fixed w-full z-20 top-0">
+        @include('products.navbarAdmin')
+    </div>
+    <!-- NAVBAR END -->
     <div class="container mt-5 mb-5">
         <div class="row">
             <div class="col-md-4">
--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,26 @@ use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\AdminController;
 
 
+
+// User View
 Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/navbar', function () {
+    return view('navbar');
+});
+
+Route::get('/rental', [ProductController::class, 'rentalProduk']);
+
+Route::get('/spesifikasi/{id}', [ProductController::class, 'showSpesifikasi'])->name('spesifikasi.show');
+
+
+
+
+// Login
 Route::get('/adminlog', function () {
     return view('AdminLogin');
-
 })->name('admin.loginview');
 
 Route::get('/forgot-password', function () {
@@ -23,42 +36,26 @@ Route::get('/forgot-password', function () {
 Route::get('reset-password', [ForgotPassword::class, 'resetPassword'])->name('auth.reset-password');
 Route::post('reset-password', [ForgotPassword::class, 'resetPassworProcess'])->name('auth.reset-password.send');
 
-Route::post('/forgot-password',[ForgotPassword::class,"index"])->name('auth.forgot-password.send');
-
 Route::post('/forgot-password', [ForgotPassword::class, "index"])->name('auth.forgot-password.send');
 
-Route::post('/admin/login', [AdminController::class, 'login'])->name('admin.login'); // Fix the route action for admin login process
+Route::post('/admin/login', [AdminController::class, 'login'])->name('admin.login');
 
+
+
+// Products
 Route::get('/admin/products', function () {
     return view('products.index');
 })->name('products.index');
-
-Route::get('/navbar', function () {
-    return view('navbar');
-});
 
 Route::get('/navbarAdmin', function () {
     return view('products.navbarAdmin');
 });
 
-Route::get('/spesifikasi', function () {
-    return view('spesifikasi');
-});
+// Route resource for products
+Route::resource('/products', ProductController::class);
 
-// Route::get('/rental', function () {
-//     return view('rental');
-// });
-
-Route::get('/rental', [ProductController::class, 'rentalProduk']);
-
-
-// Route::get('/Transaksi', [TransaksiController::class, 'index']);
+// Transaksi
 Route::get('/Transaksi', [TransaksiController::class, 'index'])->name('transaksi.index');
 Route::post('/Transaksi', [TransaksiController::class, 'store'])->name('transaksi.store');
 Route::get('/create', [TransaksiController::class, 'create'])->name('transaksi.create');
 Route::delete('/Transaksi/{id}', [TransaksiController::class, 'destroy'])->name('transaksi.destroy');
-
-
-
-//route resource for products
-Route::resource('/products', \App\Http\Controllers\ProductController::class);
